4183003685aaba4563340cad2f8440cbdac96208
Ricardo A. Ferrari <ricardoaugusto.ferrari@gmail.com>
2025-02-22 17:41:09 -0300

    feat: template switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'template-reflection';
+  myContext = {$implicit: 'World', value: 'Svet'};
 
+  items = [
+    {name: 'First', value: 1, type: 'bold'},
+    {name: 'Second', value: 2, type: 'italic'},
+    {name: 'Third', value: 3, type: 'bold'},
+    {name: 'Fourth', value: 4, type: 'italic'},
+  ];
 }
